6aae646453c33e8912d9656525cdbcecf2626eea
Sornapriyadas <sornapriyadas@gmail.com>
2026-05-29 11:16:09 +0530

    Initial commit - Personal Portfolio Website

---
+++ script.js
@@ -0,0 +1,31 @@
+const text = [
+  "Frontend Developer",
+  "Web Designer",
+  "Java Learner",
+  "Intern at Pinnacle Labs"
+];
+
+let count = 0;
+let index = 0;
+let currentText = "";
+let letter = "";
+
+(function type(){
+
+  if(count === text.length){
+    count = 0;
+  }
+
+  currentText = text[count];
+  letter = currentText.slice(0, ++index);
+
+  document.getElementById('typing').textContent = letter;
+
+  if(letter.length === currentText.length){
+    count++;
+    index = 0;
+  }
+
+  setTimeout(type,150);
+
+})();
